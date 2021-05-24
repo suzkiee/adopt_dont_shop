d9044ebcadead2894582c582caa71a5afd284b46
Suzie K <suziekim.dev@gmail.com>
2021-05-24 09:56:50 -0400

    Update db to fix zip_code bug

--- db/migrate/20210524133720_create_applications.rb
+++ db/migrate/20210524133720_create_applications.rb
@@ -1,13 +1,13 @@
 class CreateApplications < ActiveRecord::Migration[5.2]
   def change
     create_table :applications do |t|
-      t.string :name  
+      t.string :name
       t.string :street_address
       t.string :city
       t.string :state
-      t.integer :zip_code
-      t.string :description
+      t.string :zip_code
       t.string :status
+      t.string :description
 
       t.timestamps
     end
