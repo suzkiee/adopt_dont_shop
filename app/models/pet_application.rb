283e4362b9c8edaea5cf14a23a74bb75306c1a47
Suzie K <suziekim.dev@gmail.com>
2021-05-26 12:47:23 -0400

    Green test ::find_pets method

--- app/models/pet_application.rb
+++ app/models/pet_application.rb
@@ -6,4 +6,8 @@ class PetApplication < ApplicationRecord
     pet_app = where("pet_id = #{pet_id} and application_id = #{app_id}")
     pet_app.first
   end
+
+  def self.find_pets(pet_id, app_id)
+    pets = where("pet_id = #{pet_id} and application_id = #{app_id}")
+  end
 end
