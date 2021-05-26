d2598cbb61da6e8fb78b8dfe932d7e636cef225f
Suzie K <suziekim.dev@gmail.com>
2021-05-26 09:30:06 -0400

    Green test ::find_application

--- app/models/pet_application.rb
+++ app/models/pet_application.rb
@@ -1,4 +1,9 @@
 class PetApplication < ApplicationRecord 
   belongs_to :pet
   belongs_to :application
+
+  def self.find_application(pet_id, app_id)
+    pet_app = where("pet_id = #{pet_id} and application_id = #{app_id}")
+    pet_app.first.application
+  end
 end
