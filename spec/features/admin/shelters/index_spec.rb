88455d7c78db21fa84a154c8ddb70e4d38240077
Suzie K <suziekim.dev@gmail.com>
2021-05-25 21:29:42 -0400

    Red test alphabetically show shelters with pending

--- spec/features/admin/shelters/index_spec.rb
+++ spec/features/admin/shelters/index_spec.rb
@@ -22,13 +22,18 @@ RSpec.describe 'the admin shelters index' do
     expect(current_path).to eq("/admin/shelters/#{@shelter_1.id}")
   end
 
-  it 'lists shelters with pending applications;' do
+  it 'lists shelters with pending applications alphabetically' do
+    pet_1 = Pet.create!(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: @shelter_3.id)
+    pet_2 = Pet.create!(adoptable: true, age: 4, breed: 'dingo', name: 'Maxy', shelter_id: @shelter_2.id)
+    app_1 = Application.create!(name: "Suzie Kim", street_address: "123 State Street", city: "Boston", state: "Masachusetts", zip_code: 02115, description: "none", status: "Pending" )
+    app_2 = Application.create!(name: "Max Bob", street_address: "123 Play Street", city: "Meford", state: "Masachusetts", zip_code: 02155, description: "none", status: "Pending" )
+    PetApplication.create!(pet: pet_1, application: app_1) 
+    PetApplication.create!(pet: pet_2, application: app_2) 
+    
     visit "/admin/shelters"
-    pet = Pet.create!(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: @shelter_1.id)
-    app = Application.create!(name: "Suzie Kim", street_address: "123 State Street", city: "Boston", state: "Masachusetts", zip_code: 02115, description: "none", status: "In Progress" )
-    pet.applications << app 
-    @shelter_1.pets << pet
 
-    expect(page).to have_content(@shelter_1.name)
+    within(".statistics") do
+      expect(@shelter_3.name).to appear_before(@shelter_2.name)
+    end
   end
 end
