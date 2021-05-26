566e4ec3dbd83e4735d252054395d4f2bbb64d02
Suzie K <suziekim.dev@gmail.com>
2021-05-25 22:04:20 -0400

    Update variable for application pet count

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -20,7 +20,8 @@ class ApplicationsController < ApplicationController
 
   def show
     @application = Application.find(params[:id])
-    
+    @pets_count = @application.pets.count
+
     if params[:adopt] && !application.pets.include?(Pet.find(params[:adopt])) 
     elsif params[:search] 
       @pets = Pet.partial_search(params[:search])
