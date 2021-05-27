6d57e7ac4aa1be2cbb01b4d192e3eb097f94856e
Suzie K <suziekim.dev@gmail.com>
2021-05-26 23:28:21 -0400

    Red test shows number of pets adopted

--- spec/features/admin/shelters/show_spec.rb
+++ spec/features/admin/shelters/show_spec.rb
@@ -4,7 +4,10 @@ require 'rails_helper'
 
 RSpec.describe 'the admin shelter show' do
   before(:each) do
-    @shelter = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+    @shelter = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+    @pet_1 = Pet.create!(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: @shelter.id)
+    @pet_2 = Pet.create!(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: @shelter.id)
+    @pet_3 = Pet.create!(adoptable: false, age: 2, breed: 'saint bernard', name: 'Beethoven', shelter_id: @shelter.id)
   end
 
   it 'shows shelter and its attributes' do
@@ -15,15 +18,27 @@ RSpec.describe 'the admin shelter show' do
   end
 
   it 'shows statistics of shelter' do
-    @shelter = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-    @pet_1 = Pet.create!(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: @shelter.id)
-    @pet_2 = Pet.create!(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: @shelter.id)
-    @pet_3 = Pet.create!(adoptable: false, age: 2, breed: 'saint bernard', name: 'Beethoven', shelter_id: @shelter.id)
 
     visit "/admin/shelters/#{@shelter.id}"
 
     expect(page).to have_content("Statistics")
     expect(page).to have_content("Average Age of All Pets: 2")
     expect(page).to have_content("Number of Adoptable Pets: 2")
+  end
+
+  it 'shows statistics of shelter' do
+    visit "/admin/shelters/#{@shelter.id}"
+
+    expect(page).to have_content("Statistics")
+    expect(page).to have_content("Average Age of All Pets: 2")
+    expect(page).to have_content("Number of Adoptable Pets: 2")
+  end
+
+  it 'shows number of pets that have been adopted from this shelter' do
+    app = Application.create!(name: "Suzie Kim", street_address: "123 State Street", city: "Boston", state: "Masachusetts", zip_code: 02115, description: "none", status: "Pending" )
+    PetApplication.create!(pet: @pet_1, application: app, status: "Accepted")
+    visit "/admin/shelters/#{@shelter.id}"
+    
+    expect(page).to have_content("Number of Pets Adopted: 1")
   end
 end 
