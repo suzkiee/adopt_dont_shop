37a11713081b73517b09c4258aaa78ad5a4ce87f
Suzie K <suziekim.dev@gmail.com>
2021-05-26 21:45:37 -0400

    Update database files

--- db/migrate/20210526135413_add_status_to_pet_application.rb
+++
@@ -1,6 +0,0 @@
-class AddStatusToPetApplication < ActiveRecord::Migration[5.2]
-  def change
-    add_column :pet_applications, :status, :string 
-    change_column_default :pet_applications, :status, 'Pending'
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_26_135714) do
+ActiveRecord::Schema.define(version: 2021_05_24_133907) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,9 +30,9 @@ ActiveRecord::Schema.define(version: 2021_05_26_135714) do
   create_table "pet_applications", force: :cascade do |t|
     t.bigint "pet_id"
     t.bigint "application_id"
+    t.string "status", default: "Pending"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "status", default: "Pending"
     t.index ["application_id"], name: "index_pet_applications_on_application_id"
     t.index ["pet_id"], name: "index_pet_applications_on_pet_id"
   end
