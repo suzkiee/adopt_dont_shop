87e2e181f70794b0ebbd806065584cc71081ada0
Suzie K <suziekim.dev@gmail.com>
2021-05-28 00:57:16 -0400

    Red test message for already adopted pets

--- spec/features/admin/applications/show_spec.rb
+++ spec/features/admin/applications/show_spec.rb
@@ -5,14 +5,9 @@ RSpec.describe 'the admin applications page'do
   before(:each) do
     @shelter = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
     @pet_1 = Pet.create!(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: @shelter.id)
-    @pet_2 = Pet.create!(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: @shelter.id)
-    @pet_3 = Pet.create!(adoptable: false, age: 2, breed: 'saint bernard', name: 'Beethoven', shelter_id: @shelter.id)
     @app = Application.create!(name: "Suzie Kim", street_address: "123 State Street", city: "Boston", state: "Masachusetts", zip_code: 02115, description: "none", status: "Pending")
     @app_2 = Application.create!(name: "Max Wannae", street_address: "123 Here Road", city: "Liliput", state: "Missouri", zip_code: 00000, description: "none", status: "Pending")
-    PetApplication.create!(pet: @pet_1, application: @app)
-    PetApplication.create!(pet: @pet_2, application: @app)
-    PetApplication.create!(pet: @pet_3, application: @app)
-    PetApplication.create!(pet: @pet_1, application: @app_2)
+    @pet_app = PetApplication.create!(pet: @pet_1, application: @app)  
   end
 
   it "shows the application and all its attributes" do
@@ -31,16 +26,17 @@ RSpec.describe 'the admin applications page'do
     
     expect(page).to have_button("Approve")
     expect(page).to have_content(@pet_1.name)
-    expect(page).to have_content(@pet_2.name)
-    expect(page).to have_content(@pet_3.name)
   end
 
   it 'can approve application to certain pet' do
+    pet_2 = Pet.create!(adoptable: true, age: 3, breed: 'doberman', name: 'Lobster', shelter_id: @shelter.id)
+    PetApplication.create!(pet: pet_2, application: @app)
+
     visit "/admin/applications/#{@app.id}"
 
     expect(page).to have_content("Pending")
 
-    first(".pets_info").click_on "Approve"
+    first(:button, "Approve").click
 
     expect(page).to have_content("Accepted")
   end
@@ -59,12 +55,10 @@ RSpec.describe 'the admin applications page'do
     visit "/admin/applications/#{@app.id}"
 
     expect(page).to have_content("Pending")
+    
+    click_on("Approve")
 
-    3.times do 
-      first(:button, 'Approve').click
-    end
-   
-    expect(page).to have_content("Accepted")
+    expect(page).to have_content("Adopted")
     expect(page).to have_content("Approved")
   end
 
@@ -72,11 +66,7 @@ RSpec.describe 'the admin applications page'do
     visit "/admin/applications/#{@app.id}"
 
     expect(page).to have_content("Pending")
-    save_and_open_page
-    2.times do 
-      first(:button, 'Approve').click
-    end
-
+    
     click_on("Reject")
    
     within('.application') do
@@ -86,21 +76,11 @@ RSpec.describe 'the admin applications page'do
   
   it 'displays message for adopted pet on other pending applications' do
     visit "/admin/applications/#{@app.id}"
-    
-    expect(@pet_1.adoptable).to eq(true)
-    expect(page).to have_content("#{@pet_1.name}")
-    
-    within('.pets_info') do
-      first(:button, 'Approve').click
-    end
 
-    expect(page).to have_content("Accepted")
-    expect(@pet_1.adoptable).to eq(false)
-
-    visit "/admin/applications/#{@app_2.id}"
-    
     expect(page).to have_content("#{@pet_1.name}")
-  
-    expect(page).to have_content("This pet has already been adopted.")
+
+    click_on('Approve')
+
+    expect(page).to have_content("Adopted")
   end
 end
