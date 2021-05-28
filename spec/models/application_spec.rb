11c7ecc2df7e3c5f436b8b9ee4b31e5b256a88f6
Suzie K <suziekim.dev@gmail.com>
2021-05-27 20:30:36 -0400

    Red test ::pending_pet_apps method

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -15,6 +15,20 @@ RSpec.describe Application, type: :model do
     it { should validate_numericality_of(:zip_code) }
   end
 
+  describe "::class methods" do
+    it '::pending_pet_apps' do
+      shelter = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+      pet_1 = Pet.create!(adoptable: true, age: 1, breed: 'sphynx', name: 'Lucille Bald', shelter_id: shelter.id)
+      pet_2 = Pet.create!(adoptable: true, age: 4, breed: 'whippet', name: 'May', shelter_id: shelter.id)
+      app_1 = Application.create!(name: "Suzie Kim", street_address: "123 State Street", city: "Boston", state: "Masachusetts", zip_code: 02115, description: "none", status: "Pending")
+      app_2 = Application.create!(name: "Suzie Kim", street_address: "123 State Street", city: "Boston", state: "New Jersey", zip_code: 07023, description: "none", status: "Pending")
+      pet_app_1 = PetApplication.create!(pet: pet_1, application: app_1)
+      pet_app_2 = PetApplication.create!(pet: pet_2, application: app_2)
+      
+      expect(Application.pending_pet_apps).to eq([app_1, app_2])
+    end
+  end
+
   describe '#instance methods' do
     it '#all_pets_accepted?' do
       shelter = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
